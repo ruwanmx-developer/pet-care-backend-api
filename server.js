918cdf5d173cdd61b2cde4d4ac632ee63db6d9f3
ruwanmx-developer <ruwanmx.developer@gmail.com>
2023-01-31 20:01:50 +0530

    create server

---
+++ server.js
@@ -0,0 +1,32 @@
+// import node modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// import project modules
+
+const userRoutes = require("./routes/user");
+
+// create app
+const app = express();
+
+// app middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// app routes
+app.use(userRoutes);
+
+// connect database
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Database Connection Error : " + err));
+
+// start application
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
